b07571ae800caaa4fc993754a743d048af3f3a99
Michał Aleksandrowicz <ma.aleksandrowicz@icloud.com>
2025-03-24 19:08:17 +0100

    add audio transcription and output JSON path add ignored files to .gitignore

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
 
 audio_dir = "data/audio"
 video_file = "data/input/Sample_MP4_1.mp4"
+transcipts_dir = "data/transcripts"
 audio_output = os.path.join(audio_dir, "output_audio.wav")
+output_json_path = os.path.join(transcipts_dir, "output_json.json")
 
 success = extract_audio(video_file, audio_output)
 
@@ -16,6 +18,14 @@ if success:
     print("Audio extraction successful! Check:", audio_output)
 else:
     print("Audio extraction failed.")
+
+working = transcribe_audio(audio_output, output_json_path)
+
+if working:
+    print("Transcription successful! Check:", output_json_path)
+else:
+    print("Transcription failed.")
+
 
 if __name__ == "__main__":
     main()
