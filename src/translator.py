9be9502307bc32697c0f2cacfcac8d090dbfc218
Michał Aleksandrowicz <ma.aleksandrowicz@icloud.com>
2025-03-31 20:53:29 +0200

    add functionality to translate transcript from text file and save to new file

--- src/translator.py
+++ src/translator.py
@@ -8,5 +8,10 @@ DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
 auth_key=DEEPL_API_KEY
 translator=deepl.Translator(auth_key)
 
-result=translator.translate_text("Hello, how are you?", target_lang="ES")
-print(result.text)
+with open("../data/transcripts/transcript.txt", "r") as file:
+    transcript = file.read()
+
+result=translator.translate_text(transcript, target_lang="ES")
+
+with open("../data/translations/translated_transcript.txt", "x") as file:
+    file.write(result.text)
