c83a57037eb0bcd1218a51521cb38bcfd00f2e50
Michał Aleksandrowicz <ma.aleksandrowicz@icloud.com>
2025-03-31 20:43:09 +0200

    add functionality to save transcript to a text file

--- src/utils.py
+++ src/utils.py
@@ -5,4 +5,7 @@ with open("../data/transcripts/output_json.json", "r") as file:
 
 transcript = data["results"]["channels"][0]["alternatives"][0]["transcript"]
 
+with open("../data/transcripts/transcript.txt", "x") as file:
+    file.write(transcript)
+
 print(transcript)
