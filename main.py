2f125d91e4352609e5932d18db016defebe377f0
Michał Aleksandrowicz <ma.aleksandrowicz@icloud.com>
2025-04-01 19:29:13 +0200

    add functionality to generate speech from translated transcript text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 from src.video_processing import extract_audio
 from src.speech_to_txt import transcribe_audio
+from src.txt_to_speech import text_to_speech
 
 
 def main():
@@ -12,21 +13,26 @@ video_file = "data/input/Sample_MP4_1.mp4"
 transcripts_dir = "data/transcripts"
 audio_output = os.path.join(audio_dir, "output_audio.wav")
 output_json_path = os.path.join(transcripts_dir, "output_json.json")
-
+""""
 success = extract_audio(video_file, audio_output)
 
 if success:
     print("Audio extraction successful! Check:", audio_output)
 else:
     print("Audio extraction failed.")
-
+"""
+"""
 working = transcribe_audio(audio_output, output_json_path)
 
 if working:
     print("Transcription successful! Check:", output_json_path)
 else:
     print("Transcription failed.")
+"""
+with open("data/translations/translated_transcript.txt", "r") as file:
+    to_generate = file.read()
 
+text_to_speech(to_generate)
 
 if __name__ == "__main__":
     main()
