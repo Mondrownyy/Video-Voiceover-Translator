0c5347afc6322b1ec275869c63808866bdc0b89a
Michał Aleksandrowicz <ma.aleksandrowicz@icloud.com>
2025-04-01 19:56:06 +0200

    add functionality to replace audio in video with generated audio

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dotenv
 from src.video_processing import extract_audio
 from src.speech_to_txt import transcribe_audio
 from src.txt_to_speech import text_to_speech
+from src.video_processing import replace_audio
 
 
 def main():
@@ -13,6 +14,9 @@ video_file = "data/input/Sample_MP4_1.mp4"
 transcripts_dir = "data/transcripts"
 audio_output = os.path.join(audio_dir, "output_audio.wav")
 output_json_path = os.path.join(transcripts_dir, "output_json.json")
+video_path = "data/input/Sample_MP4_1.mp4"
+audio_path = "data/generated_audio/3041ddec-12de-4b7c-8654-b4dc1db1dbc5.mp3"
+output_path = "data/output/output_video.mp4"
 """"
 success = extract_audio(video_file, audio_output)
 
@@ -29,10 +33,14 @@ if working:
 else:
     print("Transcription failed.")
 """
+""""
 with open("data/translations/translated_transcript.txt", "r") as file:
     to_generate = file.read()
 
 text_to_speech(to_generate)
+"""
+
+replace_audio(video_path, audio_path, output_path)
 
 if __name__ == "__main__":
     main()
